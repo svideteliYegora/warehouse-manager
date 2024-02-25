469accc25bcd2a4c5141b5cc5bb18246cec28c68
Egor K <112383203+GoshaCatharsis@users.noreply.github.com>
2024-02-25 15:29:33 +0300

    - update files

--- dialog_3.py
+++ dialog_3.py
@@ -1,9 +1,8 @@
 import PyQt5
 from BD import workBD, con
 from PyQt5 import QtCore, QtGui, QtWidgets
-from product_detail import Ui_ProductDetail
-from user_detail import Ui_UserDetail
-from user_cart import Ui_UserCart
+from PyQt5.QtGui import QPixmap
+import sqlite3
 
 
 class Ui_Dialog(object):
@@ -240,7 +239,7 @@ class Ui_Dialog(object):
 
     def download(self) -> None:
         """
-        Метод для заполнения таблицы.
+        Метод для загрузки продуктов по выбранному складу.
 
         :return: None
         """
@@ -345,10 +344,6 @@ class Ui_Dialog(object):
         # заполнение таблицы tableWidget_2
         self.table_filling(data, self.tableWidget_2)
 
-
-
-
-
     def filling_combo_boxes(self) -> None:
         """
         Метод для заполнения комбо боксов
@@ -411,6 +406,257 @@ class Ui_Dialog(object):
                 item = QtWidgets.QTableWidgetItem(str(itm[i]))
                 table_widget.setItem(row, i-1, item)
 
+
+class Ui_ProductDetail(object):
+    product_id = None
+
+    def setupUi(self, Dialog):
+        Dialog.setObjectName("Dialog")
+        Dialog.resize(614, 433)
+        self.label = QtWidgets.QLabel(Dialog)
+        self.label.setGeometry(QtCore.QRect(50, 120, 230, 230))
+        self.label.setText("")
+        self.label.setObjectName("label")
+        self.pushButton = QtWidgets.QPushButton(Dialog)
+        self.pushButton.setGeometry(QtCore.QRect(490, 380, 93, 28))
+        self.pushButton.setObjectName("pushButton")
+        self.label_2 = QtWidgets.QLabel(Dialog)
+        self.label_2.setGeometry(QtCore.QRect(40, 70, 250, 250))
+        self.label_2.setText("")
+        self.label_2.setObjectName("label_2")
+        self.textEdit = QtWidgets.QTextEdit(Dialog)
+        self.textEdit.setGeometry(QtCore.QRect(330, 70, 250, 250))
+        self.textEdit.setObjectName("textEdit")
+
+        self.retranslateUi(Dialog)
+        QtCore.QMetaObject.connectSlotsByName(Dialog)
+        self.pushButton.clicked.connect(lambda: Dialog.close())
+
+    def retranslateUi(self, Dialog):
+        _translate = QtCore.QCoreApplication.translate
+        Dialog.setWindowTitle(_translate("Dialog", "Dialog"))
+
+        # получение информацию о товаре
+        product: tuple = workBD.get_product_cart(self.product_id)
+        id_, name, category, characteristic, vendor_code, price, img_path = product
+        pixmap = QPixmap(img_path)
+        self.label_2.setPixmap(pixmap)
+
+        # масштабирование изображения
+        self.label_2.setScaledContents(True)
+
+        # формирование и вставка текста
+        text = "<b>Название</b>: {}<br><br>" \
+               "<b>Категория</b>: {}<br><br>" \
+               "<b>Артикул</b>: {}<br><br>" \
+               "<b>Стоимость</b>: {}<br><br>" \
+               "<b>Характеристика</b>:<br> {}". \
+            format(name, category, vendor_code, price, characteristic)
+        self.textEdit.setReadOnly(True)
+        self.textEdit.setText(text)
+        self.pushButton.setText(_translate("Dialog", "Выход"))
+
+
+class Ui_UserCart(object):
+    def setupUi(self, Dialog):
+        Dialog.setObjectName("Dialog")
+        Dialog.resize(410, 531)
+        self.dialog = Dialog
+        self.lineEdit = QtWidgets.QLineEdit(Dialog)
+        self.lineEdit.setGeometry(QtCore.QRect(20, 40, 371, 31))
+        self.lineEdit.setText("")
+        self.lineEdit.setObjectName("lineEdit")
+        self.lineEdit_2 = QtWidgets.QLineEdit(Dialog)
+        self.lineEdit_2.setGeometry(QtCore.QRect(20, 110, 371, 31))
+        self.lineEdit_2.setObjectName("lineEdit_2")
+        self.lineEdit_3 = QtWidgets.QLineEdit(Dialog)
+        self.lineEdit_3.setGeometry(QtCore.QRect(20, 180, 371, 31))
+        self.lineEdit_3.setObjectName("lineEdit_3")
+        self.lineEdit_4 = QtWidgets.QLineEdit(Dialog)
+        self.lineEdit_4.setGeometry(QtCore.QRect(20, 250, 371, 31))
+        self.lineEdit_4.setObjectName("lineEdit_4")
+        self.lineEdit_5 = QtWidgets.QLineEdit(Dialog)
+        self.lineEdit_5.setGeometry(QtCore.QRect(20, 320, 371, 31))
+        self.lineEdit_5.setObjectName("lineEdit_5")
+        self.lineEdit_6 = QtWidgets.QLineEdit(Dialog)
+        self.lineEdit_6.setGeometry(QtCore.QRect(20, 390, 371, 31))
+        self.lineEdit_6.setObjectName("lineEdit_6")
+        self.label = QtWidgets.QLabel(Dialog)
+        self.label.setGeometry(QtCore.QRect(20, 10, 71, 31))
+        self.label.setObjectName("label")
+        self.label_2 = QtWidgets.QLabel(Dialog)
+        self.label_2.setGeometry(QtCore.QRect(20, 80, 171, 31))
+        self.label_2.setObjectName("label_2")
+        self.label_3 = QtWidgets.QLabel(Dialog)
+        self.label_3.setGeometry(QtCore.QRect(20, 150, 101, 31))
+        self.label_3.setObjectName("label_3")
+        self.label_4 = QtWidgets.QLabel(Dialog)
+        self.label_4.setGeometry(QtCore.QRect(20, 220, 71, 31))
+        self.label_4.setObjectName("label_4")
+        self.label_5 = QtWidgets.QLabel(Dialog)
+        self.label_5.setGeometry(QtCore.QRect(20, 290, 81, 31))
+        self.label_5.setObjectName("label_5")
+        self.label_6 = QtWidgets.QLabel(Dialog)
+        self.label_6.setGeometry(QtCore.QRect(20, 360, 71, 31))
+        self.label_6.setObjectName("label_6")
+        self.horizontalLayoutWidget = QtWidgets.QWidget(Dialog)
+        self.horizontalLayoutWidget.setGeometry(QtCore.QRect(20, 470, 371, 41))
+        self.horizontalLayoutWidget.setObjectName("horizontalLayoutWidget")
+        self.horizontalLayout = QtWidgets.QHBoxLayout(self.horizontalLayoutWidget)
+        self.horizontalLayout.setContentsMargins(0, 0, 0, 0)
+        self.horizontalLayout.setObjectName("horizontalLayout")
+        self.pushButton = QtWidgets.QPushButton(self.horizontalLayoutWidget)
+        self.pushButton.setObjectName("pushButton")
+        self.horizontalLayout.addWidget(self.pushButton)
+        self.pushButton_2 = QtWidgets.QPushButton(self.horizontalLayoutWidget)
+        self.pushButton_2.setObjectName("pushButton_2")
+        self.horizontalLayout.addWidget(self.pushButton_2)
+        self.label_7 = QtWidgets.QLabel(Dialog)
+        self.label_7.setGeometry(QtCore.QRect(20, 430, 401, 20))
+        self.label_7.setObjectName("label_7")
+        self.label_7.setVisible(False)
+
+        self.pushButton.clicked.connect(self.create)
+        self.pushButton_2.clicked.connect(lambda: self.dialog.close())
+
+        self.retranslateUi(Dialog)
+        QtCore.QMetaObject.connectSlotsByName(Dialog)
+
+    def retranslateUi(self, Dialog):
+        _translate = QtCore.QCoreApplication.translate
+        Dialog.setWindowTitle(_translate("Dialog", "Dialog"))
+        self.label.setText(_translate("Dialog", "<html><head/><body><p><span style=\" font-size:10pt;\">Имя*</span></p></body></html>"))
+        self.label_2.setText(_translate("Dialog", "<html><head/><body><p><span style=\" font-size:10pt;\">Фамилия*</span></p></body></html>"))
+        self.label_3.setText(_translate("Dialog", "<html><head/><body><p><span style=\" font-size:10pt;\">Отчество*</span></p></body></html>"))
+        self.label_4.setText(_translate("Dialog", "<html><head/><body><p><span style=\" font-size:10pt;\">Возраст*</span></p></body></html>"))
+        self.label_5.setText(_translate("Dialog", "<html><head/><body><p><span style=\" font-size:10pt;\">Адрес*</span></p></body></html>"))
+        self.label_6.setText(_translate("Dialog", "<html><head/><body><p><span style=\" font-size:10pt;\">Email*</span></p></body></html>"))
+        self.pushButton.setText(_translate("Dialog", "Cоздать"))
+        self.pushButton_2.setText(_translate("Dialog", "Отменить"))
+        self.label_7.setText(_translate("Dialog", "<html><head/><body><p><span style=\" font-size:7pt; color:#ff0000;\">Для добавления новой записи необходимо заполнить все поля!</span></p></body></html>"))
+
+    def create(self) -> None:
+        """
+        Создание записи нового пользователя.
+
+        :return: None.
+        """
+        if self.checker_fields():
+            try:
+                workBD.add_record(
+                    "users",
+                    first_name=self.lineEdit.text(),
+                    last_name=self.lineEdit_2.text(),
+                    surname=self.lineEdit_3.text(),
+                    age=self.lineEdit_4.text(),
+                    address=self.lineEdit_5.text(),
+                    email=self.lineEdit_6.text()
+                )
+                self.dialog.close()
+            except sqlite3.OperationalError as e:
+                print(e)
+                self.label_7.setVisible(True)
+                self.label_7.setText(
+                    "<html><head/><body><p><span style=\" font-size:7pt; color:#ff0000;\">Ошибка: Запись не была добавлена. Повторите попытку позже.</span></p></body></html>")
+            except Exception as e:
+                print(e)
+        else:
+            self.label_7.setVisible(True)
+
+    def checker_fields(self) -> bool:
+        """
+        Метод проверяет корректность заполненных полей, возвращает True, если да и False, в противном случае.
+        :return: None.
+        """
+        if all(
+            [
+                self.lineEdit.text(), self.lineEdit_2.text(),
+                self.lineEdit_3.text(), self.lineEdit_4.text(),
+                self.lineEdit_5.text(), self.lineEdit_6.text()
+            ]
+        ):
+            return True
+        else:
+            return False
+
+
+class Ui_UserDetail(object):
+    user_id = None
+
+    def setupUi(self, Dialog):
+        Dialog.setObjectName("Dialog")
+        Dialog.resize(614, 433)
+        self.groupBox = QtWidgets.QGroupBox(Dialog)
+        self.groupBox.setGeometry(QtCore.QRect(20, 150, 571, 231))
+        self.groupBox.setObjectName("groupBox")
+        self.tableWidget = QtWidgets.QTableWidget(self.groupBox)
+        self.tableWidget.setGeometry(QtCore.QRect(10, 20, 551, 192))
+        self.tableWidget.setObjectName("tableWidget")
+        self.tableWidget.setColumnCount(4)
+        self.tableWidget.setRowCount(0)
+        item = QtWidgets.QTableWidgetItem()
+        self.tableWidget.setHorizontalHeaderItem(0, item)
+        item = QtWidgets.QTableWidgetItem()
+        self.tableWidget.setHorizontalHeaderItem(1, item)
+        item = QtWidgets.QTableWidgetItem()
+        self.tableWidget.setHorizontalHeaderItem(2, item)
+        item = QtWidgets.QTableWidgetItem()
+        self.tableWidget.setHorizontalHeaderItem(3, item)
+        self.groupBox_2 = QtWidgets.QGroupBox(Dialog)
+        self.groupBox_2.setGeometry(QtCore.QRect(20, 10, 301, 131))
+        self.groupBox_2.setObjectName("groupBox_2")
+        self.textEdit = QtWidgets.QTextEdit(self.groupBox_2)
+        self.textEdit.setGeometry(QtCore.QRect(10, 20, 281, 101))
+        self.textEdit.setObjectName("textEdit")
+        self.pushButton = QtWidgets.QPushButton(Dialog)
+        self.pushButton.setGeometry(QtCore.QRect(470, 390, 121, 23))
+        self.pushButton.setObjectName("pushButton")
+
+        self.retranslateUi(Dialog)
+        QtCore.QMetaObject.connectSlotsByName(Dialog)
+        self.pushButton.clicked.connect(lambda: Dialog.close())
+
+    def retranslateUi(self, Dialog):
+        _translate = QtCore.QCoreApplication.translate
+        Dialog.setWindowTitle(_translate("Dialog", "Dialog"))
+        self.groupBox.setTitle(_translate("Dialog", "Список заказов:"))
+        item = self.tableWidget.horizontalHeaderItem(0)
+        item.setText(_translate("Dialog", "Номер заказа"))
+        item = self.tableWidget.horizontalHeaderItem(1)
+        item.setText(_translate("Dialog", "Стоимость заказа"))
+        item = self.tableWidget.horizontalHeaderItem(2)
+        item.setText(_translate("Dialog", "Дата заказа"))
+        item = self.tableWidget.horizontalHeaderItem(3)
+        item.setText(_translate("Dialog", "Содержимое"))
+        self.groupBox_2.setTitle(_translate("Dialog", "Данные пользователя:"))
+        self.pushButton.setText(_translate("Dialog", "Выход"))
+
+        # получение данных пользователя
+        user = workBD.get_user_cart(self.user_id)
+        id_, name, lastname, surname, age, address, email = user
+
+        # формирование и вставка текста
+        text = "<b>Фамилия</b>: {}<br>" \
+               "<b>Имя</b>: {}<br>" \
+               "<b>Отчество</b>: {}<br>" \
+               "<b>Возраст</b>: {} лет<br>" \
+               "<b>Адрес</b>: {}<br>" \
+               "<b>Email</b>: {}".\
+            format(lastname, name, surname, age, address, email)
+        self.textEdit.setReadOnly(True)
+        self.textEdit.setText(text)
+
+        # получение информации о заказах пользователя
+        orders = workBD.get_user_orders(self.user_id)
+        # заполнение таблицы
+        self.tableWidget.setRowCount(len(orders))
+        for row, order in enumerate(orders):
+
+            for i in range(len(order)):
+                item = QtWidgets.QTableWidgetItem(str(order[i]))
+                self.tableWidget.setItem(row, i, item)
+        # установка запрета на редактирование на изменение таблицы
+        self.tableWidget.setEditTriggers(QtWidgets.QAbstractItemView.NoEditTriggers)
 
 
 if __name__ == "__main__":
